d8e8694605a31fdae7c7e95ee85eb144b0b71878
André Lagacione <lagacioneandre@gmail.com>
2024-03-03 19:50:03 -0500

    fix messages

--- src/services/ValidationTokenService.ts
+++ src/services/ValidationTokenService.ts
@@ -7,7 +7,7 @@ class ValidationTokenService {
     public async execute({ token }: IValidationToken) {
         const tokenRepository = AppDataSource.getRepository(ValidationToken);
         const registeredToken = await tokenRepository.findOneBy({ token });
-        if (!registeredToken) throw new AppError('TOKEN_NOT_FOUND', 404);
+        if (!registeredToken) throw new AppError('API_ERRORS.TOKEN_NOT_FOUND', 404);
         const currentDateInMilliseconds = new Date().getTime();
         const validateTokenTime = new Date(registeredToken.validateTokenTime).getTime();
         if (currentDateInMilliseconds > validateTokenTime || registeredToken.validated) throw new AppError('API_ERRORS.TOKEN_EXPIRED');
